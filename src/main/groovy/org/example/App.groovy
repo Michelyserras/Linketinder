721103cbd1539b6611a0f814a38666c09dbde606
Michelyserras <Michelysrrs@gmail.com>
2025-09-14 18:57:10 -0300

    Removendo pastas de build do controle de versão

---
+++ src/main/groovy/org/example/App.groovy
@@ -0,0 +1,8 @@
+package org.example
+
+import org.example.view.MainView
+
+static void main(String[] args) {
+    MainView mainView = new MainView()
+    mainView.menu()
+}
---
+++ src/main/groovy/org/example/view/PessoaJuridicaView.groovy
@@ -0,0 +1,97 @@
+package org.example.view
+
+import org.example.Controller.PessoaJuridicaController
+import org.example.Model.PessoaJuridica
+
+class PessoaJuridicaView {
+    private PessoaJuridicaController pessoaJuridicaController = new PessoaJuridicaController()
+    static scanner = new Scanner(System.in)
+
+    void menuPessoaJuridica() {
+        Scanner scanner = new Scanner(System.in)
+
+        while (true) {
+            println """
+        1 - Cadastrar Pessoa Juridica
+        2 - Listar Pessoas Juridicas
+        3 - Voltar
+        """
+
+            print "Escolha uma opcao: "
+            def input = scanner.nextLine().trim()
+            int opcao = -1
+            try {
+                opcao = input.toInteger()
+            } catch (Exception e) {
+                println "Entrada inválida! Digite um número de 1 a 3."
+                continue
+            }
+
+            switch (opcao) {
+                case 1:
+                    limpaTela()
+                    cadastrarEmpresa()
+                    break
+                case 2:
+                    limpaTela()
+                    listarEmpresas()
+                    break
+                case 3:
+                    limpaTela()
+                    return
+                default:
+                    println "Opcao invalida! Digite 1, 2 ou 3."
+            }
+        }
+    }
+
+
+    void cadastrarEmpresa(){
+        String nome, descricao, cnpj, pais, email,  cep, estado
+        List<String> competencias = new ArrayList<>()
+
+        println "Digite o nome: "
+        nome = scanner.nextLine()
+        println "Digite o email: "
+        email = scanner.nextLine()
+        println "Digite o cep: "
+        cep = scanner.nextLine()
+        println "Digite o estado: "
+        estado = scanner.nextLine()
+        println "Digite a descricao: "
+        descricao = scanner.nextLine()
+        println "Digite o cnpj: "
+        cnpj = scanner.nextLine()
+        println "Digite o pais: "
+        pais = scanner.nextLine()
+        for(int i = 0; i < 3; i++){
+            println "Digite a competencia ${i+1}: "
+            String competencia = scanner.nextLine()
+            competencias.add(competencia)
+        }
+
+        PessoaJuridica pessoaJuridica =  new PessoaJuridica(nome, email, cep, estado, descricao, cnpj, pais, competencias)
+        try {
+            pessoaJuridicaController.cadastrarPessoaJuridica(pessoaJuridica)
+            println "Pessoa Juridica cadastrada com sucesso!"
+        } catch (Exception e) {
+            println "Erro ao cadastrar Pessoa Juridica: ${e.message}"
+        }
+    }
+
+    void listarEmpresas(){
+        try{
+             pessoaJuridicaController.listarPessoaJuridica()
+        } catch (Exception e) {
+            println "Erro ao listar Pessoas Juridicas: ${e.message}"
+        }
+    }
+
+    void limpaTela(){
+        for(int i = 0; i < 50; i++){
+            println ""
+        }
+    }
+
+
+}
